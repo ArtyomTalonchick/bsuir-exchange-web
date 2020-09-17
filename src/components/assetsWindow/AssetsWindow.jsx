3c370155227f21bc79ec90022afcbf24a6cc25bb
artyom <artyomtalonchick@gmail.com>
2020-09-17 22:48:21 +0300

    Implemented AssetsWindow

--- src/components/assetsWindow/AssetsWindow.jsx
+++ src/components/assetsWindow/AssetsWindow.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TextField} from '@material-ui/core';
+import {TextField, MenuItem} from '@material-ui/core';
 
 import Window from '../window/Window';
 import withAlert from '../withAlert';
@@ -8,15 +8,43 @@ import withLoader from '../withLoader';
 import validateHelper from '../../helpers/validateHelper';
 
 import './AssetsWindow.scss';
+import card1 from '../../static/card1.jpg';
+import card2 from '../../static/card2.jpg';
+import card3 from '../../static/card3.jpg';
+
+const DEFAULT_STATE = {
+    currencies: [],
+    // fields
+    owner: '',
+    cvv: '',
+    number: '',
+    month: '01',
+    year: '2020',
+    amount: '',
+    currency: '',
+}
 
 class AssetsWindow extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = JSON.parse(JSON.stringify(DEFAULT_STATE));
     }
 
-    onChangeField = e => this.setState({[e.currentTarget.name]: e.currentTarget.value})
+    componentDidUpdate(prevProps) {
+        if (!prevProps.open && this.props.open) {
+            this.props.startLoading();
+            const currencies = [{id: 0, name: 'USD'}, {id: 1, name: 'EUR'}, {id: 1, name: 'BTC'}];
+            setTimeout(() => {
+                this.setState({currencies, currency: currencies[0]?.id});
+                this.props.finishLoading();
+            }, 500);
+        } else if (prevProps.open && !this.props.open) {
+            this.setState(JSON.parse(JSON.stringify(DEFAULT_STATE)));
+        }
+    }
+
+    onChangeField = e => this.setState({[e.target.name]: e.target.value})
 
     onSubmit = () => {
         return;
@@ -43,17 +71,101 @@ class AssetsWindow extends React.Component {
                 onCancel={this.props.onClose}
                 onSubmit={this.onSubmit}
                 submitDisabled={submitDisabled}
-                title='Create account'
+                title='Top up balance'
             >
-                <div>
+                <div className='_padding'>
+                    <div className='_df-aic-jcsb'>
+                        <TextField
+                            error={!validateHelper.bankCardOwner(this.state.owner)}
+                            label='Owner'
+                            variant='outlined'
+                            name='owner'
+                            value={this.state.owner}
+                            onChange={this.onChangeField}
+                            className='bank-card__field _owner'
+                            fullWidth
+                        />
+                        <TextField
+                            error={!validateHelper.bankCardCvv(this.state.cvv)}
+                            label='CVV'
+                            variant='outlined'
+                            name='cvv'
+                            value={this.state.cvv}
+                            onChange={this.onChangeField}
+                            className='bank-card__field _cvv'
+                            fullWidth
+                        />
+                    </div>
+
                     <TextField
-                        error={!validateHelper.password(this.state.name)}
-                        label='Password'
+                        error={!validateHelper.bankCardNumber(this.state.number)}
+                        label='Card number'
                         variant='outlined'
-                        name='name'
-                        value={this.state.name}
+                        name='number'
+                        value={this.state.number}
                         onChange={this.onChangeField}
+                        className='bank-card__field _number'
                     />
+
+                    <div className='bank-card__date'>
+                        <TextField
+                            select
+                            label='Month'
+                            name='month'
+                            value={this.state.month}
+                            onChange={this.onChangeField}
+                        >
+                            {['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'].map((v) => (
+                                <MenuItem key={v} value={v}>
+                                    {v}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            select
+                            label='Year'
+                            name='year'
+                            value={this.state.year}
+                            onChange={this.onChangeField}
+                            className='bank-card__field _year'
+                        >
+                            {['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2080', '2029'].map((v) => (
+                                <MenuItem key={v} value={v}>
+                                    {v}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+
+                        <img src={card1} alt=''/>
+                        <img src={card2} alt=''/>
+                        <img src={card3} alt=''/>
+                    </div>
+
+                    <div className='_df-aic-jcsb bank-card__values'>
+                        <TextField
+                            error={!validateHelper.bankCardAmount(this.state.amount)}
+                            label='Amount'
+                            variant='outlined'
+                            name='amount'
+                            value={this.state.amount}
+                            onChange={this.onChangeField}
+                            fullWidth
+                        />
+                        <TextField
+                            select
+                            label='Currency'
+                            name='currency'
+                            value={this.state.currency}
+                            onChange={this.onChangeField}
+                            className='bank-card__field _currency'
+                        >
+                            {this.state.currencies.map(({id, name}) => (
+                                <MenuItem key={id} value={id}>
+                                    {name}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </div>
                 </div>
             </Window>
         );
--- src/helpers/validateHelper.js
+++ src/helpers/validateHelper.js
@@ -3,4 +3,8 @@ export default {
     email: value => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value),
     password: value => /^[A-Za-z\d]\w{7,14}$/.test(value),
     accountName: value => /^.{3,12}$/.test(value),
+    bankCardOwner: value => /^[a-zA-Z]{2,}[ ][a-zA-Z]{2,}$/.test(value),
+    bankCardCvv: value => /^\d{3}$/.test(value),
+    bankCardNumber: value => /^\d{16}$/.test(value),
+    bankCardAmount: value => /^\d{1,10}$/.test(value),
 }
