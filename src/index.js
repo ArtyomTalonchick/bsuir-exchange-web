7ec47e4b74392581cdaaf26f81ff599bb209415e
artyom <artyomtalonchick@gmail.com>
2020-09-20 16:36:19 +0300

    Disable console errors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.scss';
 import App from './App';
+import './helpers/consoleHelper';
 
 ReactDOM.render(
   <React.StrictMode>
