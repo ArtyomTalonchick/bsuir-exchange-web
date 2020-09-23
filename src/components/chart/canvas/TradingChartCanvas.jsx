eaa766327070537c59f022e3f4403d9317e7207a
artyom <artyomtalonchick@gmail.com>
2020-09-23 21:04:36 +0300

    Fixed wheel event on chart

--- src/components/chart/canvas/TradingChartCanvas.jsx
+++ src/components/chart/canvas/TradingChartCanvas.jsx
@@ -13,10 +13,22 @@ import {EdgeIndicator} from 'react-stockcharts/lib/coordinates';
 
 import {ORDER_SIDES} from '../../../constants/constants';
 
+const CHART_ID = 'CHART_ID';
+
 class TradingChartCanvas extends React.Component {
 
+    componentDidMount() {
+        document.getElementById(CHART_ID).addEventListener('wheel', this.wheelHandler);
+    }
+
+    componentWillUnmount() {
+        document.getElementById(CHART_ID).removeEventListener('wheel', this.wheelHandler);
+    }
+
+    wheelHandler = e => e.preventDefault()
+
     getColor = () => this.props.side === ORDER_SIDES.BUY ? '#1B5E20' : '#BF360C';
-    
+
     getCanvasGradient = () => createVerticalLinearGradient([
         {stop: 0, color: hexToRGBA(this.getColor(), 0.1)},
         {stop: 0.7, color: hexToRGBA(this.getColor(), 0.3)},
@@ -35,41 +47,43 @@ class TradingChartCanvas extends React.Component {
 
     render() {
         return (
-            <ChartCanvas
-                // type='svg'
-                ratio={1}
-                width={this.props.width}
-                height={400}
-                margin={{left: 50, right: 50, top: 10, bottom: 30}}
-                data={this.props.data}
-                xAccessor={d => d.date}
-                xScale={scaleTime()}
-                // clamp={true}
-            >
-                <Chart yExtents={(d) => d.price}>
-                    <HoverTooltip
-                        stroke={hexToRGBA('#415757', 1)}
-                        fill={hexToRGBA('#415757', 0.5)}
-                        fontFill={hexToRGBA('#FFFFFF', 1)}
-                        bgFill={hexToRGBA('#415757', 0.1)}
-                        tooltipContent={this.getTooltip}
-                    />
-                    <EdgeIndicator
-                        itemType='last'
-                        orient='right'
-                        edgeAt='right'
-                        yAccessor={d => d.price}
-                        fill={hexToRGBA('#415757', 1)}
-                    />
-                    <XAxis axisAt='bottom' orient='bottom' ticks={5}/>
-                    <YAxis axisAt='left' orient='left'/>
-                    <AreaSeries
-                        stroke={hexToRGBA(this.getColor(), 1)}
-                        yAccessor={d => d.price}
-                        canvasGradient={this.getCanvasGradient()}
-                    />
-                </Chart>
-            </ChartCanvas>
+            <div id={CHART_ID}>
+                <ChartCanvas
+                    // type='svg'
+                    ratio={1}
+                    width={this.props.width}
+                    height={400}
+                    margin={{left: 50, right: 50, top: 10, bottom: 30}}
+                    data={this.props.data}
+                    xAccessor={d => d.date}
+                    xScale={scaleTime()}
+                    // clamp={true}
+                >
+                    <Chart yExtents={(d) => d.price}>
+                        <HoverTooltip
+                            stroke={hexToRGBA('#415757', 1)}
+                            fill={hexToRGBA('#415757', 0.5)}
+                            fontFill={hexToRGBA('#FFFFFF', 1)}
+                            bgFill={hexToRGBA('#415757', 0.1)}
+                            tooltipContent={this.getTooltip}
+                        />
+                        <EdgeIndicator
+                            itemType='last'
+                            orient='right'
+                            edgeAt='right'
+                            yAccessor={d => d.price}
+                            fill={hexToRGBA('#415757', 1)}
+                        />
+                        <XAxis axisAt='bottom' orient='bottom' ticks={5}/>
+                        <YAxis axisAt='left' orient='left'/>
+                        <AreaSeries
+                            stroke={hexToRGBA(this.getColor(), 1)}
+                            yAccessor={d => d.price}
+                            canvasGradient={this.getCanvasGradient()}
+                        />
+                    </Chart>
+                </ChartCanvas>
+            </div>
         );
     }
 }
