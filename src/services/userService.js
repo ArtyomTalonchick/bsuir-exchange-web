2230eba5e92406bdebb9ca80e1281eabc9786a63
artyom <artyomtalonchick@gmail.com>
2020-09-26 09:53:40 +0300

    Implemented email verification

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,24 +2,32 @@ import {RestRequest} from './requestService';
 import {endpoints} from '../constants/endpoints';
 import {MODULES} from '../constants/loadingModules';
 import {finishLoading, startLoading} from '../providers/loaderProvider';
-import {setError} from '../providers/alertsProvider';
+import {setError, setSuccess} from '../providers/alertsProvider';
 import {setUser} from '../providers/userProvider';
 import {setAccounts} from '../providers/accountsProvider';
 import {setAssets} from '../providers/assetsProvider';
 
-const _login = (username, password) => RestRequest.post(endpoints.user.login, {},{username, password});
+const _login = (username, password) => RestRequest.post(endpoints.user.login, {}, {username, password});
 
-const _registration = (username, password) => RestRequest.post(endpoints.user.registration, {},{username, password});
+const _registration = (username, password) => RestRequest.post(endpoints.user.registration, {}, {username, password});
 
 const auth = (method, username, password) => {
     startLoading(MODULES.USER);
     return method(username, password)
         .then(response => {
-            setUser(JSON.parse(response.data));
+            const data = JSON.parse(response.data);
+            if (response.status === 200) {
+                setUser(data);
+            } else {
+                setSuccess(data.message);
+            }
             return true;
         })
         .catch(reason => {
-            reason.response.status !== 401 && setError('Server error, sorry, try again later');
+            const message = reason.response.status === 409
+                ? 'User with such mail already exists'
+                : 'Server error, sorry, try again later';
+            reason.response.status !== 401 && setError(message);
             return false;
         })
         .finally(() => finishLoading(MODULES.USER));
@@ -29,17 +37,17 @@ const registration = (username, password) => auth(_registration, username, passw
 
 const login = (username, password) => auth(_login, username, password);
 
-
 const update = () => {
     if (!localStorage.getItem('Authorization')) return;
 
     startLoading(MODULES.USER);
-    return RestRequest.get(endpoints.user.info, {},{})
+    return RestRequest.get(endpoints.user.info, {}, {})
         .then(response => {
             setUser(JSON.parse(response.data));
             return true;
         })
-        .catch(() => {})
+        .catch(() => {
+        })
         .finally(() => finishLoading(MODULES.USER));
 }
 
