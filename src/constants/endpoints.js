17b06c2ad2fd917860c20d3a44c5404d72a88a12
artyom <artyomtalonchick@gmail.com>
2020-09-26 09:57:17 +0300

    Fixed apiUrl

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
-// const apiUrl = 'https://bsuir-exchange-api.herokuapp.com/';
-const apiUrl = 'http://127.0.0.1:8000/';
+const apiUrl = 'https://bsuir-exchange-api.herokuapp.com/';
+// const apiUrl = 'http://127.0.0.1:8000/';
 
 export const endpoints = {
     user: {
